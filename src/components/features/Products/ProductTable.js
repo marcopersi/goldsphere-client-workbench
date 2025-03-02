7e761c77d1e200124a4bcdb08e8eab50b14f406e
Marco Persi <marcopersi@me.com>
2025-03-02 16:04:51 +0100

    code optimization

--- src/components/features/Products/ProductTable.js
+++ src/components/features/Products/ProductTable.js
@@ -1,40 +1,46 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import '../../../styles/ProductTable.css';
 
-const ProductTable = ({ products, quantities, handleQuantityChange, totalSum, t }) => (
-  <table className="product-table">
-    <thead>
-      <tr>
-        <th>{t('quantity')}</th>
-        <th>{t('productname')}</th>
-        <th>{t('price')}</th>
-        <th>{t('total')}</th>
-      </tr>
-    </thead>
-    <tbody>
-      {products.map(product => (
-        <tr key={product.id}>
-          <td>
-            <input
-              type="number"
-              min="1"
-              value={quantities[product.id] || 1}
-              onChange={(e) => handleQuantityChange(product.id, parseInt(e.target.value, 10))}
-            />
-          </td>
-          <td>{product.productname}</td>
-          <td>{product.price}</td>
-          <td>{(quantities[product.id] || 1) * parseFloat(product.price)}</td>
+const ProductTable = ({ products, quantities, handleQuantityChange, totalSum, t }) => {
+  const handleInputChange = useCallback((productId, value) => {
+    handleQuantityChange(productId, parseInt(value, 10));
+  }, [handleQuantityChange]);
+
+  return (
+    <table className="product-table">
+      <thead>
+        <tr>
+          <th>{t('quantity')}</th>
+          <th>{t('productname')}</th>
+          <th>{t('price')}</th>
+          <th>{t('total')}</th>
         </tr>
-      ))}
-      <tr>
-        <td colSpan="3" style={{ textAlign: "right", color: "white" }}>{t('total')}</td>
-        <td>{totalSum.toFixed(2)}</td>
-      </tr>
-    </tbody>
-  </table>
-);
+      </thead>
+      <tbody>
+        {products.map(product => (
+          <tr key={product.id}>
+            <td>
+              <input
+                type="number"
+                min="1"
+                value={quantities[product.id] || 1}
+                onChange={(e) => handleInputChange(product.id, e.target.value)}
+              />
+            </td>
+            <td>{product.productname}</td>
+            <td>{product.price}</td>
+            <td>{(quantities[product.id] || 1) * parseFloat(product.price)}</td>
+          </tr>
+        ))}
+        <tr>
+          <td colSpan="3" style={{ textAlign: "right", color: "white" }}>{t('total')}</td>
+          <td>{totalSum.toFixed(2)}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
 
 ProductTable.propTypes = {
   products: PropTypes.arrayOf(PropTypes.shape({
